accb1aaf0494eb2b9a735b4da719645b01075ec2
rpeters1430 <47281316+rpeters1430@users.noreply.github.com>
2025-05-19 19:48:06 -0700

    udpates

--- app/src/main/java/com/example/jellyfinryan/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/jellyfinryan/ui/screens/HomeScreen.kt
@@ -1,3 +1,4 @@
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,6 +14,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -28,12 +30,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.tv.material3.Card
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
 import coil.compose.AsyncImage
 import com.example.jellyfinryan.viewmodel.HomeViewModel
-import androidx.compose.material3.CardDefaults
 
 
 @Composable
