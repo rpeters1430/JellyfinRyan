7dd5b57a9acd656a63e32bf8d05ac8a8e7948a82
rpeters1430 <47281316+rpeters1430@users.noreply.github.com>
2025-05-21 18:33:15 -0700

    Revert "updates"
    This reverts commit d97e71a195d7ba514f0640fe83e6a6722cf2d9b5.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,8 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    plugins {
-        id("com.android.application") version "8.2.0" apply false
-        id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-        id("dagger.hilt.android.plugin") version "2.47" apply false
-        id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
-    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -18,5 +13,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
 rootProject.name = "JellyfinTV"
 include(":app")
